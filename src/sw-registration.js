57ae0171b1524c4f3a5d0fda628ae05c0e684ff4
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-03-24 23:58:58 +0100

    fixed: path to sw registration

--- src/sw-registration.js
+++ src/sw-registration.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('sw.js')
   })
 }
