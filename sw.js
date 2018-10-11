bc2e4789aa228a4fbcdf736eb60da3555d5ebb97
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-10-11 23:22:04 +0200

    removed: firebaseio from the sw

--- sw.js
+++ sw.js
@@ -3,22 +3,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js')
 
 workbox.routing.registerRoute(
-  /.*(?:firebase)\.io.*$/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'api-cache',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
-      }),
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200]
-      })
-    ]
-  }),
-)
-
-workbox.routing.registerRoute(
   /.*(?:googleapis|gstatic)\.com.*$/,
   workbox.strategies.staleWhileRevalidate(),
 )
